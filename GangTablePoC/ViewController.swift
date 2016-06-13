97432a8079afec7b06aa28b063cf4a5be4df62f7
Ricki Gregersen <hi@rickigregersen.com>
2016-06-13 20:43:29 +0200

    Initial Commit

---
+++ GangTablePoC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GangTablePoC
+//
+//  Created by Ricki Gregersen on 13/06/16.
+//  Copyright © 2016 youandthegang.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
